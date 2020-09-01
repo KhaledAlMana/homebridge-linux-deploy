c0268ddae47b8cdfaaa4b5afe51cbbbe04fa6bd2
Khaled Al Mana <ikhaledim@gmail.com>
2020-09-01 20:51:02 +0300

    Script added

---
+++ startup-homebridge.sh
@@ -0,0 +1,54 @@
+#!bin/bash
+
+main(){
+	SCRIPT_NAME="startup-homebridge.sh";
+	ALLOWED_INSTANCES=2
+
+	echo SCRIPT_NAME=$SCRIPT_NAME;
+
+	echo "script instances running:"
+	get_script $SCRIPT_NAME;
+
+        echo "script instances running [bash]:"
+	get_bash_script $SCRIPT_NAME
+
+	RUNNING=$(get_bash_script $SCRIPT_NAME);
+	if [ $RUNNING -gt $ALLOWED_INSTANCES ]
+	   then
+        	echo "script is already running";
+		exit 1;
+	   else
+	        homebridge_runner
+	fi
+}
+
+#################### START* homebridge #######################
+homebridge_runner(){
+	# Check if Homebridge UI is running since it runs with homebridge itself and homebridge port changes but its UI does not change
+	while [ true ] ; do
+		FOUND_HOMEBRIDGE=$(netstat -ltnp | grep -w ':8581' )
+		if [ "$FOUND_HOMEBRIDGE" == "" ]; then
+			pkill -9 node;
+			homebridge -I -U /var/lib/homebridge >> /var/lib/homebridge/homebridge.log
+			sleep 300;
+		fi
+	done;
+}
+#################### END* homebridge #######################
+
+
+#################### START* script restrictions #######################
+get_bash_script() {
+  pgrep -lf "(^|/)bash( | .*/)$1( |\$)" | wc -l
+}
+
+get_script() {
+  pgrep -lf ".[ /]$1( |\$)"
+}
+
+#################### END* script restrictions #######################
+
+
+
+main
+
